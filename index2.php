42e393597e3e5189be4ec27375225dba6e775e8d
Enrico-Tabilin <enrico.tabilin@stu.ocsb.ca>
2024-09-26 14:14:06 +0000

    Unit 1-07 Extra

--- index2.php
+++ index2.php
@@ -9,12 +9,12 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>form jumpscare</title>
+    <title>second index jumpscare</title>
   </head>
 
   <body>
     <!-- Uses a header that scrolls with the text, rather than staying
-      locked at the top -->
+            locked at the top -->
     <div class="mdl-layout mdl-js-layout">
       <header class="mdl-layout__header mdl-layout__header--scroll">
         <div class="mdl-layout__header-row">
@@ -42,17 +42,53 @@
       </div>
       <main class="mdl-layout__content">
         <div class="page-content">
+          <!-- content -->
           <h1>Hello, World!</h1>
-          <!-- Simple Textfield -->
-          <form action="handler.php" method="POST">
-            <div class="mdl-textfield mdl-js-textfield">
-              <input class="mdl-textfield__input" type="text" id="sample1" name="userName">
-              <label class="mdl-textfield__label" for="sample1">Name:</label>
-            </div><!-- Raised button with ripple -->
-            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" type="submit">
-              Submit
+          <p>This is a test page for these two buttons. The intention is to have alerts after pressing it.</p>
+          <div>
+            <!-- Raised button with ripple -->
+            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" id="button1" type="button">
+              Example 1
             </button>
-          </form>
+            <div id="demo-toast-example" class="mdl-js-snackbar mdl-snackbar">
+              <div class="mdl-snackbar__text"></div>
+              <button class="mdl-snackbar__action" type="button"></button>
+            </div>
+            <script>
+            (function() {
+              'use strict';
+              var snackbarContainer = document.querySelector('#demo-toast-example');
+              var showToastButton = document.querySelector('#button1');
+              showToastButton.addEventListener('click', function() {
+                'use strict';
+                var data = {message: 'Boo!'};
+                snackbarContainer.MaterialSnackbar.showSnackbar(data);
+              });
+            }());
+            </script>
+          </div>
+          <div>
+            <!-- Raised button with ripple -->
+            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" id="button2" type="button">
+              Example 2
+            </button>
+            <div id="demo-toast-example" class="mdl-js-snackbar mdl-snackbar">
+              <div class="mdl-snackbar__text"></div>
+              <button class="mdl-snackbar__action" type="button"></button>
+            </div>
+            <script>
+            (function() {
+              'use strict';
+              var snackbarContainer = document.querySelector('#demo-toast-example');
+              var showToastButton = document.querySelector('#button2');
+              showToastButton.addEventListener('click', function() {
+                'use strict';
+                var data = {message: 'Peak-a-boo!'};
+                snackbarContainer.MaterialSnackbar.showSnackbar(data);
+              });
+            }());
+            </script>
+          </div>
         </div>
       </main>
     </div>
